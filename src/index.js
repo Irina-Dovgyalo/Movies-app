5fa324edf5e3e793a53ab259fce24ac167e3fcf6
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-03-20 16:34:28 +0300

    fix: change response and add function getMovies

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const getMovies = async (url) => {
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}`)
+    }
+    
+    const body = await res.json();
+    return body;
+}
 
-fetch('https://reactjs-cdp.herokuapp.com/movies')
-.then((response) => {
-    console.log('Got response', response.statusText);
-})
+getMovies('https://reactjs-cdp.herokuapp.com/movies')
+    .then((body) => {
+        console.log(body);
+    })
+    .catch((err) => {
+        console.error('Could not fetch', err);
+    })
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
