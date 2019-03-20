38a62df1765e71c52f07ed840479bfc00219f540
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-03-20 15:56:38 +0300

    feat: get response from reactjs-cdp.herokuapp.com/

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+fetch('https://reactjs-cdp.herokuapp.com/movies')
+.then((response) => {
+    console.log('Got response', response.statusText);
+})
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
